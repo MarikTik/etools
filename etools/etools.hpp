f410ef3963f5e9aa11c896aa4e6b10fbb32de60e
Mark <mtik.philosopher@gmail.com>
2025-08-07 13:32:39 -0700

    created etools.hpp header container

---
+++ etools/etools.hpp
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+/**
+* @file etools.hpp
+* 
+* @defgroup etools
+*
+* @author Mark Tikhonov <mtik.philosopher@gmail.com>
+*
+* @date 2025-08-07
+*
+* @copyright
+* MIT License
+* Copyright (c) 2025 Mark Tikhonov
+* See the accompanying LICENSE file for details.
+*/
+#ifndef ETOOLS_ETOOLS_HPP_
+#define ETOOLS_ETOOLS_HPP_
+#include "meta/meta.hpp"
+#include "memory/memory.hpp"
+#endif //ETOOLS_ETOOLS_HPP_
